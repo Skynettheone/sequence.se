3952ba7d85dad84c221bbd178a3048f287eebfa3
thev1ndu <itsthw9@gmail.com>
2025-12-03 16:30:18 +0530

    fix

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
   name: "Sequence3",
   url: "https://sequence3.se",
-  description: "Your template for building AI-powered agents with Magic UI.",
+  description: "AI-driven Conversation Workspace",
   links: {
-    twitter: "https://x.com/dillionverma",
-    github: "https://github.com/dillionverma",
+    twitter: "https://x.com/sequence3",
+    github: "https://github.com/sequence3",
   },
 };
 
